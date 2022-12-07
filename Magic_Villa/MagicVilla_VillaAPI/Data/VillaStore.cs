d2cc53c0b4d102c2dbc3119c73b8fd95e6f586a2
myo-i <Imiyo0521@gmail.com>
2022-12-07 23:54:30 +0900

    🔥 VillaStoreをVillas（DBのテーブル）に変更するため削除

--- Magic_Villa/MagicVilla_VillaAPI/Data/VillaStore.cs
+++ Magic_Villa/MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -1,13 +1,13 @@
-﻿using MagicVilla_VillaAPI.Dto;
+﻿//using MagicVilla_VillaAPI.Dto;
 
-namespace MagicVilla_VillaAPI.Data
-{
-    public static class VillaStore
-    {
-        public static List<VillaDto> villaList = new List<VillaDto>{
-                new VillaDto{ Id = 1, Name="Pool View", Sqft=100, Occupancy=4},
-                new VillaDto{ Id = 2, Name="Beach View", Sqft=300, Occupancy=3}
-        };
+//namespace MagicVilla_VillaAPI.Data
+//{
+//    public static class VillaStore
+//    {
+//        public static List<VillaDto> villaList = new List<VillaDto>{
+//                new VillaDto{ Id = 1, Name="Pool View", Sqft=100, Occupancy=4},
+//                new VillaDto{ Id = 2, Name="Beach View", Sqft=300, Occupancy=3}
+//        };
 
-    }
-}
+//    }
+//}
